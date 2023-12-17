82befa48fa3e3caf8bc6581a4e5c22eab065f2f0
Zoher Rangwala <153892122+zoherr@users.noreply.github.com>
2023-12-17 07:46:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,13 +39,37 @@ function toggleMenu() {
 });
 
 
-// Loading Animation
-window.addEventListener("load", () => {
-    const Loader = document.querySelector(".loader");
+// // Loading Animation
+// window.addEventListener("load", () => {
+//     const Loader = document.querySelector(".loader");
 
-    Loader.classList.add("loader-hidden");
+//     Loader.classList.add("loader-hidden");
 
-    Loader.addEventListener("transitionend", () => {
-        document.body.removeChild("loader");
-    })
-})
+//     Loader.addEventListener("transitionend", () => {
+//         document.body.removeChild("loader");
+//     })
+// })
+gsap.fromTo(
+    ".loading-page",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      display: "none",
+      duration: 1.5,
+      delay: 3.5,
+    }
+  );
+  
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
